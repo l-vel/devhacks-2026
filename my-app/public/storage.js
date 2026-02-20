1f02f973ef3055e65564b2cf9e797c276b4b5537
Robert Matthew V. Andico <robert_andico@outlook.com>
2026-02-20 16:36:59 -0600

    get recent dev

---
+++ my-app/public/storage.js
@@ -0,0 +1,23 @@
+
+async function loadWordList() {
+    const url = chrome.runtime.getURL("spanish.json");
+    const res = await fetch(url);
+
+    if (!res.ok) throw new Error(`Failed to load spanish.json.`);
+
+
+    const wordList = await res.json();   
+ 
+    const map = new Map();
+    for (const entry of wordList) {
+        map.set(entry.word, entry);
+    }
+
+    return map;
+ 
+}
+
+
+
+
+
