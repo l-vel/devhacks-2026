f516e1d31a45b3f6c2f2b83e806c85435332a81c
Shaa1345 <Shaa1345@icloud.com>
2026-02-20 16:34:56 -0600

    Small styling changes

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,12 +1,42 @@
-import './App.css'
+import "./App.css";
+import { useState } from 'react'
 
 function App() {
-
+    const [knownWords] = useState(0)
+    const [unknownWords] = useState(0)
   return (
     <div>
-      HI
+      <header style = {{fontSize: '150%'}}>Spanish To Ingles~</header>
+      <div style={{ display: "flex" }}>
+        <div style={{ margin: "8px" }}>
+          <div
+            style={{
+              padding: "3px",
+              fontSize: "300%",
+            }}
+          >
+            {knownWords}
+          </div>
+          <div text-xs="" c-text-tertiary="" font-semibold="">
+            Known words
+          </div>
+        </div>
+        <div style={{ margin: "8px" }}>
+          <div
+            style={{
+              padding: "3px",
+              fontSize: "300%",
+            }}
+          >
+            {unknownWords}
+          </div>
+          <div text-xs="" c-text-tertiary="" font-semibold="">
+            Seen words
+          </div>
+        </div>
       </div>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
